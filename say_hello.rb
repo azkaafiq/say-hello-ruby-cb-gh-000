d06913bb4f5ae7775c78980f6dfc07b91ddd5086
AHMAD AZKA AFIQ AHMAD KAMRAN <azkarucoo@hotmail.com>
2019-08-27 11:33:10 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,9 @@
 # Build your say_hello method here
 
-def say_hello(name = "Ruby programmer")
-  puts "Hello, #{name}"
+def say_hello(name)
+puts "Hello Kent Beck"
+end
+
+ def say_hello(name="Ruby Programmer!")
+  puts ("Hello #{name}!")
 end
